afcb322b831c6f1e7af22308fffe7d370575fa86
minthing <121234836+milliekang@users.noreply.github.com>
2023-04-03 22:33:43 +0900

    first styled components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+const Parent = styled.div`
+display: flex;
+`
+
+const ChildOne = styled.div`
+width: 100px;
+height: 100px;
+background-color:lavender;
+`
+
+const ChildTwo = styled.div`
+width: 100px;
+height: 100px;
+background-color:lightcoral;
+`
+
+const Text = styled.h3``
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <Parent>
+    <ChildOne>
+      <Text>hello</Text>
+    </ChildOne>
+    <ChildTwo />
+  </Parent>);
 }
 
 export default App;
