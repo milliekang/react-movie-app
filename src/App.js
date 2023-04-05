c08230321276708a009e85cd26e88c413a0d0b67
minthing <121234836+milliekang@users.noreply.github.com>
2023-04-05 20:35:57 +0900

    as and attrs

--- src/App.js
+++ src/App.js
@@ -9,7 +9,16 @@ width: 100px;
 height: 100px;
 background-color:${(props) => props.bgColor};
 `
+const Button = styled.button`
+color: white;
+background: tomato;
+border-radius:50px;
+border:0;
+`
 
+const Input = styled.input.attrs({required:true, minLength:10})`
+background: tomato;
+`
 
 // styled component의 함수형 사용
 const Circle = styled(Child)`
@@ -29,8 +38,13 @@ function App() {
   <Parent>
     <Child bgColor="lavender">
       <Text>hello</Text>
+      <Button>Log in</Button>
+      <Button as={"a"} href="#">Log in</Button>
     </Child>
     <Circle bgColor="whitesmoke" />
+    <Input />
+    <Input />
+    <Input />
   </Parent>);
 }
 
